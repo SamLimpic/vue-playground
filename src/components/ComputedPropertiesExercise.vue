2931e5ab30f0407eef4f6b24ed3b4ae2936b0886
SamLimpic <samlimpic@gmail.com>
2021-04-19 14:23:47 -0600

    finished

--- src/components/ComputedPropertiesExercise.vue
+++ src/components/ComputedPropertiesExercise.vue
@@ -4,8 +4,9 @@
       <div class="col-12">
         <div class="border p-1">
           <div class="reverse-message">
-            <p>Original Message: {{}}</p>
-            <p>Computed Reversed Message: {{}}</p>
+            <input type="text" v-model="state.myMessage" />
+            <p>Original Message: {{ state.myMessage }}</p>
+            <p>Computed Reversed Message: {{ state.reversedMessage }}</p>
           </div>
         </div>
       </div>
@@ -21,10 +22,11 @@ export default {
   setup() {
     const state = reactive({
       // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
+      myMessage: "",
       message: "Hello World!",
       reversedMessage: computed(() => {
         // all computed properties must return a value, what do you want this one to return?
-        return "";
+        return state.myMessage.split("").reverse().join("");
       }),
     });
     return {
